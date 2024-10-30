b031345a69f3604181687cf80173f9cdcafdf2cd
xuhe4716 <xuhe4716@uni.sydney.edu.au>
2024-10-30 22:02:45 +0800

    commit

--- RAG_Chatbot/RAG_ChatBot.py
+++ RAG_Chatbot/RAG_ChatBot.py
@@ -14,6 +14,9 @@ from langchain_pinecone import PineconeVectorStore
 import ollama
 from openai import OpenAI
 from langchain_openai import ChatOpenAI
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
 
 
 class ChatBot:
@@ -21,9 +24,10 @@ class ChatBot:
     def __init__(self):
 
         # Load and split documents
-        loader = TextLoader('./materials/Rag_document.txt')
+        loader = TextLoader('./materials/chinese.txt')
+        # loader = TextLoader('./materials/torontoTravelAssistant.txt')
         documents = loader.load()
-        text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=40)
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=40)
         docs = text_splitter.split_documents(documents)
 
 
@@ -35,29 +39,24 @@ class ChatBot:
             model_name=model_name,
             model_kwargs=model_kwargs,
             encode_kwargs=encode_kwargs
-            #query_instruction="为这个句子生成表示以用于检索相关文章："
         )
-        #embeddings.query_instruction = "为这个句子生成表示以用于检索相关文章："
-        #embeddings = HuggingFaceEmbeddings()
 
+        # Initial vector db
+        index = faiss.IndexFlatL2(len(embeddings.embed_query("hello world")))
 
-        # Initialize Pinecone instance
-        pc = Pinecone(api_key= os.getenv('PINECONE_API_KEY'))
+        vector_store = FAISS(
+            embedding_function=embeddings,
+            index=index,
+            docstore=InMemoryDocstore(),
+            index_to_docstore_id={},
+        )
 
-        index_name = "langchain-demo"
+        vector_dir = "vector"
+        if not os.path.exists(f'./materials/{vector_dir}'):
+            os.makedirs(vector_dir)
+            print(f"Created directory: {vector_dir}")
 
-        if index_name not in pc.list_indexes().names():
-            pc.create_index(
-                name=index_name,
-                dimension=768,
-                metric="cosine",
-                spec=ServerlessSpec(
-                    cloud="aws",
-                    region="us-east-1"
-                )            
-            )
-        index = pc.Index(index_name)
-        docsearch = PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name)
+        vector_store.save_local('./materials/vector')
 
         # Initialize ChatOpenAI
         model_name = "gpt-3.5-turbo"
@@ -68,19 +67,28 @@ class ChatBot:
         template = """
         You are a museum assistant for ancient Chinese characters. Users will ask you questions about Chinese characters. Use the following piece of context to answer the question.
         You should tell the user the 3 义项描述 of the word, and corresponding 语料 examples of the character.
+        You should give the answer in English. Just like the following examples:
+        Question: What does 爱 mean?
+        Answer: Meanings: \
+                1.Like, hobby (喜爱，爱好)\
+                2.love, favor, admire, love (爱护，加惠，钦慕，爱戴)\
+                Examples: \
+                1.[爱]此沧江闲白鸥。([Love] The white gulls idle in the vast river.)\
+                2.[爱]好人物，善诱无倦，士类以此高之。(He likes people and is tireless in his efforts to persuade them, and scholars admire him for this.)
         If you don't know the answer, just say you don't know.
-        Your answer should be concise.
-
+        Your answer should be precise.
+        
         Context: {context}
         Question: {question}
         Answer:
         """
 
         prompt = PromptTemplate(template=template, input_variables=["context", "question"])
-        
+
         self.rag_chain = RetrievalQA.from_chain_type(
-            llm, retriever=docsearch.as_retriever(), chain_type_kwargs={"prompt": prompt}
+            llm, retriever=vector_store.as_retriever(), chain_type_kwargs={"prompt": prompt}
         )
+
 
         
 # Usage example:
